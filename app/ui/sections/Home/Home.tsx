246afb36904c935e5acff6a76658de6f5b4a1e5c
Lukas Romsicki <lukas@romsicki.com>
2020-11-07 22:54:37 -0500

    Add example of SCSS module

--- app/ui/sections/Home/Home.tsx
+++ app/ui/sections/Home/Home.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import * as styles from './Home.module.scss';
+
 export default function Home() {
-  return <div>Hello world!</div>;
+  return <div className={styles.Home}>Hello world!</div>;
 }
