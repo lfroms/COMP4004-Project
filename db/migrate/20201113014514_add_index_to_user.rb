42b05d53b7188d3848edb222b747541c3abcda54
Gaby Pancu <gaby.pancu@shopify.com>
2020-11-13 09:17:10 -0500

    [CODE] linting

--- db/migrate/20201113014514_add_index_to_user.rb
+++ db/migrate/20201113014514_add_index_to_user.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 class AddIndexToUser < ActiveRecord::Migration[6.0]
   def change
-    add_index :users, :email, unique: true
+    add_index(:users, :email, unique: true)
   end
 end
