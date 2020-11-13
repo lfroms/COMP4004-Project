105ccbbd7e5590341a6760f5102d0525502f0d80
Mitchell Soares <mitchell.soares@shopify.com>
2020-11-12 22:09:10 -0500

    Added Apollo client + automatic graphql type generation

--- app/ui/foundation/App/App.tsx
+++ app/ui/foundation/App/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import { Routes } from 'foundation';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+      'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]')?.getAttribute('token'),
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <Routes />
+      </Router>
+    </ApolloProvider>
   );
 }
