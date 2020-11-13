5be76efb5be27562d8ae6411ea5a5ff96ef4cb17
Gaby Pancu <gaby.pancu@shopify.com>
2020-11-12 19:50:12 -0500

    add db seed that produces one admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(name: 'Jane', email: 'jane@email.com', password: '123456', admin: true, approved: true)
+p "Created #{User.count} user(s)"
