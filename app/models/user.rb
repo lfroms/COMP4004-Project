3baf1e134bf1ce35406b145386d96a6912c65df6
Gaby Pancu <gaby.pancu@shopify.com>
2020-11-12 20:32:17 -0500

    [CODE] add uniqueness validation on email of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, format: URI::MailTo::EMAIL_REGEXP, presence: true
+  validates :email, format: URI::MailTo::EMAIL_REGEXP, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
 end
