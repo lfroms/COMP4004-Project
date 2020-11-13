731384cc7d4decae1452772cf7d4101fe21fe81a
Lukas Romsicki <lukas@romsicki.com>
2020-11-12 22:19:40 -0500

    [CODE] Add implementation for UserAuthenticator

---
+++ app/services/json_web_token/user_authenticator.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+module JsonWebToken
+  class UserAuthenticator
+    class << self
+      def validate(request_headers)
+        @request_headers = request_headers
+
+        begin
+          payload = JsonWebToken::Coder.decode(token)
+
+          User.find_by(id: payload['user_id'])
+
+        rescue => _
+          nil
+        end
+      end
+
+      private
+
+      def token
+        @request_headers['Authorization'].split(' ').last
+      end
+    end
+  end
+end
