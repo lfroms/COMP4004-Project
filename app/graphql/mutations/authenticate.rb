071967bc73b021439729e5e100f7dbdce3076b32
Lukas Romsicki <lukas@romsicki.com>
2020-11-14 17:41:21 -0500

    [CODE] Return GraphQL error for unapproved login attempt

--- app/graphql/mutations/authenticate.rb
+++ app/graphql/mutations/authenticate.rb
@@ -9,6 +9,11 @@ module Mutations
     def resolve(email:, password:)
       user = User.find_by(email: email).try(:authenticate, password)
 
+      if user
+        raise GraphQL::ExecutionError,
+          'This account has not yet been approved. Please try again later.' unless user.approved
+      end
+
       {
         token: user ? JsonWebToken::Coder.encode({ user_id: user.id }) : nil,
       }
