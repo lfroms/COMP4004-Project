b5b9d46a4364da9e4f05c92ffc0ec7184cc92437
Lukas Romsicki <lukas@romsicki.com>
2020-11-14 17:13:11 -0500

    [CODE] Create basic data structure for defining menus

--- app/ui/components/Page/index.ts
+++ app/ui/components/Page/index.ts
@@ -1,1 +1,2 @@
 export { default as Page } from './Page';
+export { Group as NavigationGroup } from './Page';
--- app/ui/sections/Home/Home.tsx
+++ app/ui/sections/Home/Home.tsx
@@ -1,9 +1,31 @@
-import { Page } from 'components';
+import { HomeOutlined } from '@ant-design/icons';
+import { NavigationGroup, Page } from 'components';
 import React from 'react';
 
 export default function Home() {
+  const groups: NavigationGroup[] = [
+    {
+      id: 'test_group',
+      title: 'Test group',
+      icon: <HomeOutlined />,
+      items: [
+        {
+          id: 'first',
+          title: 'First test item',
+          onSelect: () => console.log('testing'),
+        },
+      ],
+    },
+  ];
+
   return (
-    <Page title="Home" subtitle="Subtitle">
+    <Page
+      title="Home"
+      subtitle="Subtitle"
+      groups={groups}
+      defaultOpenGroupId="test_group"
+      defaultSelectedItemId="first"
+    >
       <div>Hello world!</div>
     </Page>
   );
