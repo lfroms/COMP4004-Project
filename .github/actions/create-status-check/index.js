596fa1a38814e7cda2492451589d8f616d1495cb
Lukas Romsicki <lukas@romsicki.com>
2020-11-14 19:15:42 -0500

    Merge status checks and report unit tests separately

---
+++ .github/actions/create-status-check/index.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+  try {
+    const [gitHubRepoOwner, gitHubRepoName] = process.env.GITHUB_REPOSITORY.split('/');
+    const gitHubSha = process.env.GITHUB_SHA;
+    const gitHubToken = core.getInput('github_token');
+
+    const octokit = new github.GitHub(gitHubToken);
+
+    octokit.checks.create({
+      owner: gitHubRepoOwner,
+      repo: gitHubRepoName,
+      name: core.getInput('name'),
+      head_sha: gitHubSha,
+      status: 'completed',
+      conclusion: core.getInput('conclusion'),
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
