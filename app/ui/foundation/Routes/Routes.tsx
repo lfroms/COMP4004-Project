9938bd8d98c2d8abe150762d068dd50de77ff9dc
Gaby Pancu <gaby.pancu@shopify.com>
2020-11-15 14:53:11 -0500

    [CODE] create Login component and add new route

--- app/ui/foundation/Routes/Routes.tsx
+++ app/ui/foundation/Routes/Routes.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import { Home } from 'sections';
+import { Login } from 'sections';
 
 export default function Routes() {
   return (
     <Switch>
+      <Route exact path="/login" component={Login} />
       <Route exact path="/" component={Home} />
     </Switch>
   );
