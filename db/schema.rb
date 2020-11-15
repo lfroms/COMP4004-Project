136d4a290c0c093fc4949798b4783d927faee16b
Lukas Romsicki <lukas@romsicki.com>
2020-11-15 15:42:25 -0500

    [CODE] Add Group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_014514) do
+ActiveRecord::Schema.define(version: 2020_11_15_203405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "can_self_enroll", default: false, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
