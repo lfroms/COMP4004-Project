d5caf4bd34681272fe1e633d7f521eeb14eb6e42
Lukas Romsicki <3951690+lfroms@users.noreply.github.com>
2020-11-15 20:15:43 -0500

    Add mutation to create new user (#97)
    * [TEST] Add tests for user creation mutation
    
    * [CODE] Add implementation for CreateUser mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 module Types
   class MutationType < Types::BaseObject
+    field :create_user, mutation: Mutations::CreateUser
     field :authenticate, mutation: Mutations::Authenticate
   end
 end
