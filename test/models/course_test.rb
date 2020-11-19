63f2aa5aaa8be64f072a0e9747317d7b466e5287
Lukas Romsicki <lukas@romsicki.com>
2020-11-19 16:11:31 -0500

    [TEST] Add tests for Course model

---
+++ test/models/course_test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class CourseTest < ActiveSupport::TestCase
+  test 'course can be created with valid name and code' do
+    course = Course.new(name: 'Software Quality Assurance', code: 'COMP 4004')
+
+    assert course.valid?
+  end
+
+  test 'course cannot be created if name is missing' do
+    course = Course.new(code: 'COMP 4004')
+
+    assert_not course.valid?
+  end
+
+  test 'course cannot be created if code is missing' do
+    course = Course.new(name: 'Software Quality Assurance')
+
+    assert_not course.valid?
+  end
+
+  test 'course is invalid if name does not match XXXX 0000' do
+    course = Course.new(name: 'Software Quality Assurance', code: 'A1B2C3')
+
+    assert_not course.valid?
+  end
+end
