fd0e7ffb3150bc858c046d2e87a56b63290e4cf7
Lukas Romsicki <lukas@romsicki.com>
2020-11-18 20:51:29 -0500

    [CODE] Fix nullability on groups resolver

--- app/graphql/resolvers/groups.rb
+++ app/graphql/resolvers/groups.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Resolvers
   class Groups < Resolvers::Base
-    type Types::GroupType.connection_type, null: true
+    type Types::GroupType.connection_type, null: false
 
     def resolve
       Group.all
