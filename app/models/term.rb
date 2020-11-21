ac5ff9e795b19742ffa0977511e1ef892af795ca
Mitchell Soares <mitchell.soares@shopify.com>
2020-11-21 11:21:32 -0500

    [CODE] Updated fixtures to be valid and fixed linting

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class Term < ApplicationRecord
   validates :start_date, :end_date, :registration_deadline, :withdrawal_deadline, :financial_deadline, presence: true
   validate :validate_start_end, if: :start_end_present?
@@ -5,18 +6,20 @@ class Term < ApplicationRecord
 
   def validate_start_end
     if start_date > end_date
-      errors.add(:start_date, "start_date must be before end_date")
+      errors.add(:start_date, 'start_date must be before end_date')
     end
   end
 
   def validate_deadlines
     if start_end_present?
       if withdrawal_deadline > end_date || withdrawal_deadline < start_date
-        errors.add(:withdrawal_deadline, "withdrawal_deadline must be within the bounds of start_date to end_date of the term")
+        errors.add(:withdrawal_deadline,
+          'withdrawal_deadline must be within the bounds of start_date to end_date of the term')
       end
 
       if financial_deadline > end_date || financial_deadline < start_date
-        errors.add(:financial_deadline, "financial_deadline must be within the bounds of start_date to end_date of the term")
+        errors.add(:financial_deadline,
+          'financial_deadline must be within the bounds of start_date to end_date of the term')
       end
     end
   end
