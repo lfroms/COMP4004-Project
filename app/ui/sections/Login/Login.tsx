810519c0c5037cac735b4ba390e4a4930c67f384
Lukas Romsicki <3951690+lfroms@users.noreply.github.com>
2020-11-21 16:15:12 -0500

    Hide main navigation frame on login screen (#146)

--- app/ui/sections/Login/Login.tsx
+++ app/ui/sections/Login/Login.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Button, Form, Input } from 'antd';
+import { Button, Card, Form, Input, Space } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { gql, useMutation } from '@apollo/client';
 import { useToken } from 'hooks';
 import { AuthenticateMutation } from './graphql/AuthenticateMutation';
+
+import * as styles from './Login.module.scss';
 
 export default function Login() {
   const AUTHENTICATE = gql`
@@ -44,36 +46,45 @@ export default function Login() {
   }
 
   return (
-    <Form name="login" onFinish={handleFinish}>
-      <Form.Item
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your email!',
-          },
-        ]}
-      >
-        <Input prefix={<UserOutlined />} placeholder="Email" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your password!',
-          },
-        ]}
-      >
-        <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-      </Form.Item>
+    <div className={styles.Login}>
+      <Card className={styles.LoginCard}>
+        <Form name="login" onFinish={handleFinish}>
+          <Form.Item
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined />} placeholder="Email" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
+          </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
+          <Form.Item>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                Log in
+              </Button>
+
+              <span>
+                Or <a href="">register now!</a>
+              </span>
+            </Space>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   );
 }
