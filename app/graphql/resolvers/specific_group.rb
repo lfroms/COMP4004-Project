759b56f5fb7a85675215e282cffaba17a1dae220
Lukas Romsicki <lukas@romsicki.com>
2020-11-21 14:53:21 -0500

    [CODE] Remove authorization until #142

--- app/graphql/resolvers/specific_group.rb
+++ app/graphql/resolvers/specific_group.rb
@@ -3,10 +3,6 @@ module Resolvers
   class SpecificGroup < Resolvers::Base
     type Types::GroupType, null: true
 
-    def ready?(*)
-      context[:current_user]&.admin
-    end
-
     def resolve(id:)
       Group.find_by(id: id)
     end
