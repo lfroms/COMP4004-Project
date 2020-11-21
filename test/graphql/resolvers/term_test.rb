913abdb18f8513203c1775e6bf1a113e52387bbd
Mitchell Soares <mitchell.soares@shopify.com>
2020-11-21 16:02:08 -0500

    [TEST] Added term and terms test

---
+++ test/graphql/resolvers/term_test.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+module Resolvers
+  class TermTest < ActiveSupport::TestCase
+    test '#resolve returns specified term' do
+      term_id = Term.last.id
+      query = <<~EOF
+        query Term {
+          term(id: #{term_id}) {
+            startDate
+            endDate
+            financialDeadline
+            registrationDeadline
+            withdrawalDeadline
+          }
+        }
+      EOF
+
+      results = CmsSchema.execute(query, context: {}, variables: {}).to_h
+      term = results.dig('data', 'term')
+      assert_equal terms(:one).start_date, term['startDate']
+      assert_equal terms(:one).end_date, term['endDate']
+      assert_equal terms(:one).financial_deadline, term['financialDeadline']
+      assert_equal terms(:one).registration_deadline, term['registrationDeadline']
+      assert_equal terms(:one).withdrawal_deadline, term['withdrawalDeadline']
+    end
+
+    test '#resolve returns nil when specified term does not exist' do
+      query = <<~EOF
+        query Term {
+          term(id: 0) {
+            startDate
+            endDate
+            financialDeadline
+            registrationDeadline
+            withdrawalDeadline
+          }
+        }
+      EOF
+
+      results = CmsSchema.execute(query, context: {}, variables: {}).to_h
+      value = results.dig('data', 'term')
+
+      assert_nil value
+    end
+  end
+end
---
+++ test/graphql/resolvers/terms_test.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+module Resolvers
+  class TermsTest < ActiveSupport::TestCase
+    test '#resolve returns all saved terms' do
+      query = <<~EOF
+        query Terms {
+          terms {
+            nodes {
+              startDate
+              endDate
+              financialDeadline
+              registrationDeadline
+              withdrawalDeadline
+            }
+          }
+        }
+      EOF
+
+      results = CmsSchema.execute(query, context: {}, variables: {}).to_h
+      terms = results.dig('data', 'terms', 'nodes')
+
+      assert_equal 2, terms.length
+      assert_equal terms(:one).start_date, terms[0]['startDate']
+      assert_equal terms(:one).end_date, terms[0]['endDate']
+      assert_equal terms(:one).financial_deadline, terms[0]['financialDeadline']
+      assert_equal terms(:one).registration_deadline, terms[0]['registrationDeadline']
+      assert_equal terms(:one).withdrawal_deadline, terms[0]['withdrawalDeadline']
+    end
+  end
+end
