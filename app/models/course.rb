996994071a11ea02447b75d6bf2e81f16cbea7f8
Lukas Romsicki <lukas@romsicki.com>
2020-11-21 19:38:33 -0500

    [CODE] Adjust Course associations to properly handle foreign keys

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,6 +3,12 @@ class Course < ApplicationRecord
   validates :name, presence: true
   validates :code, presence: true, format: { with: /[A-Z]{4} [0-9]{4}/ }
 
-  has_many :course_prerequisites, class_name: 'Prerequisite', dependent: :destroy
+  has_many :course_prerequisites,
+    class_name: 'Prerequisite',
+    foreign_key: :prerequisite_id,
+    dependent: :destroy,
+    inverse_of: :course
+
   has_many :prerequisites, through: :course_prerequisites, source: :prerequisite
+  has_many :offerings, dependent: :destroy
 end
