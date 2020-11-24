52933b6de53ccca571d647f670229782e40083ba
Lukas Romsicki <lukas@romsicki.com>
2020-11-23 22:52:07 -0500

    [CODE] Update Frame to add menu items

--- app/ui/components/Frame/Frame.tsx
+++ app/ui/components/Frame/Frame.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 import { Layout, Menu } from 'antd';
-import { ControlOutlined } from '@ant-design/icons';
+import { BookOutlined, ControlOutlined } from '@ant-design/icons';
 
 import * as styles from './Frame.module.scss';
 
@@ -9,11 +9,21 @@ interface Props {
   children: React.ReactNode;
 }
 
+enum MenuItem {
+  'admin',
+  'courses',
+}
+
 export default function Frame(props: Props) {
   const { children } = props;
   const location = useLocation();
+  const history = useHistory();
 
-  if (location.pathname.match(/login/)) {
+  const pathnameMatches = (string: string) => {
+    return location.pathname.includes(string);
+  };
+
+  if (pathnameMatches('/login')) {
     return (
       <div className={styles.Frame}>
         <Layout.Content>{children}</Layout.Content>
@@ -21,14 +31,32 @@ export default function Frame(props: Props) {
     );
   }
 
+  const getSelectedKey: () => MenuItem = () => {
+    if (pathnameMatches('/admin')) return MenuItem.admin;
+    if (pathnameMatches('/courses')) return MenuItem.courses;
+
+    return MenuItem.courses;
+  };
+
   return (
     <div className={styles.Frame}>
       <Layout.Sider collapsed theme="dark" className={styles.GlobalNav}>
         <div className={styles.Logo} />
 
-        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-          <Menu.Item key="1" icon={<ControlOutlined />}>
+        <Menu theme="dark" mode="inline" selectedKeys={[getSelectedKey().toString()]}>
+          <Menu.Item
+            key={MenuItem.admin}
+            icon={<ControlOutlined />}
+            onClick={() => history.push('/admin')}
+          >
             Admin
+          </Menu.Item>
+          <Menu.Item
+            key={MenuItem.courses}
+            icon={<BookOutlined />}
+            onClick={() => history.push('/courses')}
+          >
+            Courses
           </Menu.Item>
         </Menu>
       </Layout.Sider>
