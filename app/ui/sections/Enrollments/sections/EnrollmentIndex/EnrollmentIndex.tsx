739c0cf171fa5e56e2d10863750af36411dce0e4
Lukas Romsicki <lukas@romsicki.com>
2020-11-24 17:15:28 -0500

    [REF] Remove temporary markup from EnrollmentIndex

--- app/ui/sections/Enrollments/sections/EnrollmentIndex/EnrollmentIndex.tsx
+++ app/ui/sections/Enrollments/sections/EnrollmentIndex/EnrollmentIndex.tsx
@@ -1,42 +1,5 @@
 import React from 'react';
-import { NavigationGroup, Page } from 'components';
-import { UserOutlined } from '@ant-design/icons';
-import { useHistory, useLocation } from 'react-router-dom';
 
 export default function EnrollmentIndex() {
-  const history = useHistory();
-  const location = useLocation();
-
-  const groups: NavigationGroup[] = [
-    {
-      id: 'users_and_groups',
-      title: 'Users and groups',
-      icon: <UserOutlined />,
-      items: [
-        {
-          id: '/admin/users',
-          title: 'Users',
-          onSelect: () => history.push('/admin/users'),
-        },
-        {
-          id: '/admin/groups',
-          title: 'Groups',
-          onSelect: () => history.push('/admin/groups'),
-        },
-      ],
-    },
-  ];
-
-  return (
-    <Page
-      title="Courses"
-      groups={groups}
-      initialOpenGroupIds={['users_and_groups']}
-      selectedItemId={
-        location.pathname.includes('/admin/users') ? '/admin/users' : location.pathname
-      }
-    >
-      <div>hi</div>
-    </Page>
-  );
+  return <div>A grid of courses currently enrolled in.</div>;
 }
