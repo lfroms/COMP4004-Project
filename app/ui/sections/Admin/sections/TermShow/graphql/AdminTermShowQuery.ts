3d46882c18a9f9ab6c0499cc5ac2cd1f137f856a
mitchmaps <42747959+mitchmaps@users.noreply.github.com>
2020-11-24 12:51:22 -0500

    [CODE] Added term show page (#159)

---
+++ app/ui/sections/Admin/sections/TermShow/graphql/AdminTermShowQuery.ts
@@ -0,0 +1,28 @@
+/* tslint:disable */
+/* eslint-disable */
+// @generated
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: AdminTermShowQuery
+// ====================================================
+
+export interface AdminTermShowQuery_term {
+  __typename: "Term";
+  startDate: any;
+  endDate: any;
+  financialDeadline: any;
+  registrationDeadline: any;
+  withdrawalDeadline: any;
+}
+
+export interface AdminTermShowQuery {
+  /**
+   * Specific details about a given term.
+   */
+  term: AdminTermShowQuery_term | null;
+}
+
+export interface AdminTermShowQueryVariables {
+  id: string;
+}
--- app/ui/sections/Admin/sections/index.ts
+++ app/ui/sections/Admin/sections/index.ts
@@ -2,3 +2,4 @@ export * from './UserIndex';
 export * from './UserShow';
 export * from './Groups';
 export * from './TermIndex';
+export * from './TermShow';
