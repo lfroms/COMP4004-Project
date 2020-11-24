5775b260a3256ab69e6b98b7a094c246b89df887
Lukas Romsicki <lukas@romsicki.com>
2020-11-24 16:16:57 -0500

    [CODE] Add Page to Offerings

--- app/ui/sections/Offerings/Offerings.tsx
+++ app/ui/sections/Offerings/Offerings.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { NavigationGroup, Page } from 'components';
+import { CalendarOutlined, UserOutlined } from '@ant-design/icons';
+import { Route, Switch, useHistory, useLocation } from 'react-router-dom';
 
 import { OfferingIndex } from './sections';
 
 export default function Offerings() {
+  const history = useHistory();
+  const location = useLocation();
+
+  const groups: NavigationGroup[] = [
+    {
+      id: 'users_and_groups',
+      title: 'Users and groups',
+      icon: <UserOutlined />,
+      items: [
+        {
+          id: '/admin/users',
+          title: 'Users',
+          onSelect: () => history.push('/admin/users'),
+        },
+        {
+          id: '/admin/groups',
+          title: 'Groups',
+          onSelect: () => history.push('/admin/groups'),
+        },
+      ],
+    },
+    {
+      id: 'terms',
+      title: 'Terms',
+      icon: <CalendarOutlined />,
+      onClick: () => history.push('/admin/terms'),
+      items: [],
+    },
+  ];
+
   return (
-    <Switch>
-      <Route exact path="/courses/all" component={OfferingIndex} />
-    </Switch>
+    <Page
+      title="Course Directory"
+      groups={groups}
+      defaultOpenGroupId="users_and_groups"
+      defaultSelectedItemId={
+        location.pathname.includes('/admin/users') ? '/admin/users' : location.pathname
+      }
+    >
+      <Switch>
+        <Route exact path="/courses/all" component={OfferingIndex} />
+      </Switch>
+    </Page>
   );
 }
--- app/ui/sections/Offerings/sections/OfferingIndex/OfferingIndex.tsx
+++ app/ui/sections/Offerings/sections/OfferingIndex/OfferingIndex.tsx
@@ -1,42 +1,5 @@
 import React from 'react';
-import { NavigationGroup, Page } from 'components';
-import { UserOutlined } from '@ant-design/icons';
-import { useHistory, useLocation } from 'react-router-dom';
 
 export default function OfferingIndex() {
-  const history = useHistory();
-  const location = useLocation();
-
-  const groups: NavigationGroup[] = [
-    {
-      id: 'users_and_groups',
-      title: 'Users and groups',
-      icon: <UserOutlined />,
-      items: [
-        {
-          id: '/admin/users',
-          title: 'Users',
-          onSelect: () => history.push('/admin/users'),
-        },
-        {
-          id: '/admin/groups',
-          title: 'Groups',
-          onSelect: () => history.push('/admin/groups'),
-        },
-      ],
-    },
-  ];
-
-  return (
-    <Page
-      title="Course Directory"
-      groups={groups}
-      defaultOpenGroupId="users_and_groups"
-      defaultSelectedItemId={
-        location.pathname.includes('/admin/users') ? '/admin/users' : location.pathname
-      }
-    >
-      <div>hi</div>
-    </Page>
-  );
+  return <div>hi</div>;
 }
