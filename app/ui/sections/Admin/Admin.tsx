ddc1c35446a4ed571c97a57d635daa9fc1d6cdba
Lukas Romsicki <lukas@romsicki.com>
2020-11-24 17:20:14 -0500

    [CODE] Fix navigation bugs in Admin

--- app/ui/sections/Admin/Admin.tsx
+++ app/ui/sections/Admin/Admin.tsx
@@ -9,19 +9,23 @@ export default function Admin() {
   const history = useHistory();
   const location = useLocation();
 
+  const pathnameMatches = (string: string) => {
+    return location.pathname.includes(string);
+  };
+
   const groups: NavigationGroup[] = [
     {
-      id: 'users_and_groups',
+      id: 'usersGroups',
       title: 'Users and groups',
       icon: <UserOutlined />,
       items: [
         {
-          id: '/admin/users',
+          id: 'users',
           title: 'Users',
           onSelect: () => history.push('/admin/users'),
         },
         {
-          id: '/admin/groups',
+          id: 'groups',
           title: 'Groups',
           onSelect: () => history.push('/admin/groups'),
         },
@@ -35,14 +39,20 @@ export default function Admin() {
     },
   ];
 
+  const getSelectedKey = () => {
+    if (pathnameMatches('/admin/users')) return 'users';
+    if (pathnameMatches('/admin/groups')) return 'groups';
+    if (pathnameMatches('/admin/terms')) return 'terms';
+
+    return 'users';
+  };
+
   return (
     <Page
       title="Admin"
       groups={groups}
-      initialOpenGroupIds={['users_and_groups']}
-      selectedItemId={
-        location.pathname.includes('/admin/users') ? '/admin/users' : location.pathname
-      }
+      initialOpenGroupIds={['usersGroups']}
+      selectedItemId={getSelectedKey()}
     >
       <Switch>
         <Route exact path="/admin/users" component={UserIndex} />
