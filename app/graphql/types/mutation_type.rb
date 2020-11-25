52a0fab6daf9d092739a2f143811ad15a1ed0a06
Lukas Romsicki <lukas@romsicki.com>
2020-11-24 20:03:00 -0500

    [CODE] Add implementation for course update mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -7,6 +7,7 @@ module Types
     field :delete_user, mutation: Mutations::DeleteUser
 
     field :create_course, mutation: Mutations::CreateCourse
+    field :update_course, mutation: Mutations::UpdateCourse
     field :delete_course, mutation: Mutations::DeleteCourse
   end
 end
