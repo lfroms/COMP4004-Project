55e51e6685c3bbcf65bf33acfc440d679ad19048
Lukas Romsicki <lukas@romsicki.com>
2020-11-24 19:46:21 -0500

    [TEST] Test that courses must have unique codes

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
   test 'course can be created with valid name and code' do
-    course = Course.new(name: 'Software Quality Assurance', code: 'COMP 4004')
+    course = Course.new(name: 'Software Quality Assurance', code: 'COMP 1928')
 
     assert course.valid?
   end
 
   test 'course cannot be created if name is missing' do
-    course = Course.new(code: 'COMP 4004')
+    course = Course.new(code: 'COMP 3523')
 
     assert_not course.valid?
   end
@@ -22,6 +22,13 @@ class CourseTest < ActiveSupport::TestCase
 
   test 'course is invalid if name does not match XXXX 0000' do
     course = Course.new(name: 'Software Quality Assurance', code: 'A1B2C3')
+
+    assert_not course.valid?
+  end
+
+  test 'course is invalid if code already taken' do
+    Course.create!(name: 'Software Quality Assurance', code: 'COMP 1234')
+    course = Course.new(name: 'Software Quality Assurance 2', code: 'COMP 1234')
 
     assert_not course.valid?
   end
