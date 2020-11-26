76570273a40b879e08d65599afe4dba673dc2f24
Lukas Romsicki <lukas@romsicki.com>
2020-11-25 21:14:49 -0500

    [CODE] Add route for OfferingShow

--- app/ui/sections/Admin/Admin.tsx
+++ app/ui/sections/Admin/Admin.tsx
@@ -3,7 +3,15 @@ import { NavigationGroup, Page } from 'components';
 import { CalendarOutlined, ClockCircleOutlined, UserOutlined } from '@ant-design/icons';
 import { Redirect, Route, Switch, useHistory, useLocation } from 'react-router-dom';
 
-import { Groups, OfferingIndex, TermIndex, TermShow, UserIndex, UserShow } from './sections';
+import {
+  Groups,
+  OfferingIndex,
+  OfferingShow,
+  TermIndex,
+  TermShow,
+  UserIndex,
+  UserShow,
+} from './sections';
 
 export default function Admin() {
   const history = useHistory();
@@ -68,6 +76,7 @@ export default function Admin() {
         <Route exact path="/admin/terms" component={TermIndex} />
         <Route exact path="/admin/terms/:termId" component={TermShow} />
         <Route exact path="/admin/offerings" component={OfferingIndex} />
+        <Route exact path="/admin/offerings/:offeringId" component={OfferingShow} />
         <Redirect path="/admin" to="/admin/users" />
       </Switch>
     </Page>
