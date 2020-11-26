7c4270ad82b12df1618e3ca0fda6a7d27f65e12c
Mitchell Soares <mitchell.soares@shopify.com>
2020-11-26 14:40:05 -0500

    [CODE] Added offering delete button to offering index

--- app/ui/sections/Admin/sections/OfferingIndex/OfferingIndex.tsx
+++ app/ui/sections/Admin/sections/OfferingIndex/OfferingIndex.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
-import { gql, useQuery } from '@apollo/client';
-import { AppstoreAddOutlined } from '@ant-design/icons';
-import { Button, Col, Row, Table, Tag, Typography } from 'antd';
+import { gql, useMutation, useQuery } from '@apollo/client';
+import { AppstoreAddOutlined, DeleteOutlined } from '@ant-design/icons';
+import { Button, Col, Popconfirm, Row, Space, Table, Tag, Typography } from 'antd';
 import { ColumnType } from 'antd/lib/table/interface';
 import { Link } from 'react-router-dom';
 import { createTermName } from 'helpers';
@@ -13,6 +13,7 @@ import {
 } from './graphql/AdminOfferingIndexQuery';
 
 import * as styles from './OfferingIndex.module.scss';
+import { AdminOfferingIndexOfferingDeletionMutation } from './graphql/AdminOfferingIndexOfferingDeletionMutation';
 
 const ALL_OFFERINGS = gql`
   query AdminOfferingIndexQuery {
@@ -34,9 +35,38 @@ const ALL_OFFERINGS = gql`
   }
 `;
 
+const DELETE_OFFERING = gql`
+  mutation AdminOfferingIndexOfferingDeletionMutation($id: ID!) {
+    deleteOffering(input: { id: $id }) {
+      offering {
+        course {
+          name
+        }
+        section
+      }
+    }
+  }
+`;
+
 export default function OfferingIndex() {
   const [offeringCreateModalVisible, setOfferingCreateModalVisible] = useState(false);
+  const [focusedOfferingId, setFocusedOfferingId] = useState('');
+
   const { data, loading } = useQuery<AdminOfferingIndexQuery>(ALL_OFFERINGS);
+  const [deleteOffering] = useMutation<AdminOfferingIndexOfferingDeletionMutation>(
+    DELETE_OFFERING,
+    {
+      refetchQueries: [{ query: ALL_OFFERINGS }],
+    }
+  );
+
+  const handleDeletionConfirm = () => {
+    deleteOffering({ variables: { id: focusedOfferingId } });
+  };
+
+  const handleDeletionCancel = () => {
+    setFocusedOfferingId('');
+  };
 
   const columns: ColumnType<AdminOfferingIndexQuery_offerings_nodes>[] = [
     {
@@ -76,7 +106,26 @@ export default function OfferingIndex() {
       key: 'actions',
       fixed: 'right',
       width: 100,
-      render: (_text, record) => <Link to={`/admin/offerings/${record.id}`}>View</Link>,
+      render: (_text, record) => (
+        <Space size="middle">
+          <Link to={`/admin/offerings/${record.id}`}>View</Link>
+          <Popconfirm
+            title="Are you sure you want to delete this user?"
+            onConfirm={handleDeletionConfirm}
+            onCancel={handleDeletionCancel}
+            okText="Confirm"
+            cancelText="Cancel"
+          >
+            <Button
+              danger
+              icon={<DeleteOutlined />}
+              onClick={() => {
+                setFocusedOfferingId(record.id);
+              }}
+            />
+          </Popconfirm>
+        </Space>
+      ),
     },
   ];
 
