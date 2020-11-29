6efb1725e5e384a168ad51bfa67f4b264c2ec5b6
Lukas Romsicki <lukas@romsicki.com>
2020-11-28 22:21:08 -0500

    Disable Layout/LineLength in step definitions

--- features/step_definitions/course_scheduling_step_defs.rb
+++ features/step_definitions/course_scheduling_step_defs.rb
@@ -15,13 +15,13 @@ Given('there exists a term {string}') do |string|
   assert has_content?(string)
 end
 
-Given('there exists a course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3| # rubocop:disable Layout/LineLength
+Given('there exists a course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3|
   assert has_content?(string)
   assert has_content?(string2)
   assert has_content?(string3)
 end
 
-Given('there does not exist a course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3| # rubocop:disable Layout/LineLength
+Given('there does not exist a course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3|
   refute has_content?(string)
   refute has_content?(string2)
   refute has_content?(string3)
@@ -39,13 +39,13 @@ When('I select term {string}') do |_string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-Then('there exists a course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3| # rubocop:disable Layout/LineLength
+Then('there exists a course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3|
   assert has_content?(string)
   assert has_content?(string2)
   assert has_content?(string3)
 end
 
-Then('there exists only one course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3| # rubocop:disable Layout/LineLength
+Then('there exists only one course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3|
   assert_equal 1, find('a', text: string).count
   assert_equal 1, find('a', text: string2).count
   assert_equal 1, find('a', text: string3).count
