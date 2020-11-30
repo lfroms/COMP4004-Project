bb03195586c39a6ce1a7c7eb0a263e81bdce3cc0
Lukas Romsicki <3951690+lfroms@users.noreply.github.com>
2020-11-30 14:42:24 -0500

    Delete unused unscheduling defs

--- features/step_definitions/course_unscheduling_step_defs.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-Then('there does not exists a course offering for course with code {string} with section {string} and term {string}') do |string, string2, string3|
-  refute session.has_content?(string)
-  refute session.has_content?(string2)
-  refute session.has_content?(string3)
-end
