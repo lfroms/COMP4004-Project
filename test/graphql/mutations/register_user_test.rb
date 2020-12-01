0dfa9aa499bbede671900e4e31577dc2938402e5
Mitchell Soares <mitchell.soares@shopify.com>
2020-12-01 11:44:09 -0500

    [TEST] Added register user test

---
+++ test/graphql/mutations/register_user_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+module Mutations
+  class RegisterUserTest < ActiveSupport::TestCase
+    test '#resolve creates a new user and saves it to the database without needing to be an admin user and authenticated' do
+      query = <<~EOF
+        mutation RegisterUser {
+          registerUser(input: {name: "Test User", email: "fred@fake.com", password: "password", admin: false}) {
+            user {
+              id
+            }
+          }
+        }
+      EOF
+
+      result = CmsSchema.execute(query, variables: {}).to_h
+      id = result.dig('data', 'registerUser', 'user', 'id')
+
+      user = User.find(id)
+
+      assert_equal user.name, 'Test User'
+      assert_equal user.email, 'fred@fake.com'
+    end
+  end
+end
