a90d3652de4f4f3e6712b391285dd89e014083cf
Lukas Romsicki <lukas@romsicki.com>
2020-12-01 17:43:34 -0500

    [CODE] Add errors_hash helper to ApplicationRecord

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def errors_hash
+    errors.messages.each_with_object({}) do |(field, messages), result|
+      result[field] = errors.full_message(field, messages.first)
+    end
+  end
 end
