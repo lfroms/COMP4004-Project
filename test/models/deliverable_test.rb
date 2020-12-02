06fce1291e5e90d76e26f594a0be393d31d79282
Lukas Romsicki <lukas@romsicki.com>
2020-12-01 19:47:44 -0500

    [TEST] Add tests for Deliverable model

---
+++ test/models/deliverable_test.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class DeliverableTest < ActiveSupport::TestCase
+  test 'deliverables can be created with valid arguments' do
+    deliverable = Deliverable.new(
+      title: 'My Deliverable',
+      description: 'Description',
+      weight: 0.5,
+      due_date: Time.zone.local(2020, 10, 1, 4, 5, 6),
+      offering: offerings(:quality_assurance_A)
+    )
+
+    assert deliverable.valid?
+  end
+
+  test 'deliverables cannot be created if title missing' do
+    deliverable = Deliverable.new(
+      description: 'Description',
+      weight: 0.5,
+      due_date: Time.zone.local(2020, 10, 1, 4, 5, 6),
+      offering: offerings(:quality_assurance_A)
+    )
+
+    assert_not deliverable.valid?
+  end
+
+  test 'deliverables cannot be created if description missing' do
+    deliverable = Deliverable.new(
+      description: 'Description',
+      weight: 0.5,
+      due_date: Time.zone.local(2020, 10, 1, 4, 5, 6),
+      offering: offerings(:quality_assurance_A)
+    )
+
+    assert_not deliverable.valid?
+  end
+
+  test 'deliverables cannot be created if weight missing' do
+    deliverable = Deliverable.new(
+      title: 'My Deliverable',
+      description: 'Description',
+      due_date: Time.zone.local(2020, 10, 1, 4, 5, 6),
+      offering: offerings(:quality_assurance_A)
+    )
+
+    assert_not deliverable.valid?
+  end
+
+  test 'deliverables cannot be created if due_date missing' do
+    deliverable = Deliverable.new(
+      title: 'My Deliverable',
+      description: 'Description',
+      weight: 0.5,
+      offering: offerings(:quality_assurance_A)
+    )
+
+    assert_not deliverable.valid?
+  end
+
+  test 'deliverables cannot be created if offering missing' do
+    deliverable = Deliverable.new(
+      title: 'My Deliverable',
+      description: 'Description',
+      weight: 0.5,
+      due_date: Time.zone.local(2020, 10, 1, 4, 5, 6),
+    )
+
+    assert_not deliverable.valid?
+  end
+
+  test 'deliverables cannot be created if weight is outside the range of 0..1' do
+    deliverable = Deliverable.new(
+      title: 'My Deliverable',
+      description: 'Description',
+      weight: 5,
+      due_date: Time.zone.local(2020, 10, 1, 4, 5, 6),
+      offering: offerings(:quality_assurance_A)
+    )
+
+    assert_not deliverable.valid?
+  end
+end
