db8e001e835f88eca16ba71ba2cc1e0abff3d08b
Lukas Romsicki <lukas@romsicki.com>
2020-12-02 21:54:30 -0500

    [TEST] Add tests for prevention of unenrolled users from accessing offering

--- test/graphql/resolvers/specific_offering_test.rb
+++ test/graphql/resolvers/specific_offering_test.rb
@@ -22,6 +22,25 @@ module Resolvers
       assert_equal offerings(:quality_assurance_A).course.code, offering.dig('course', 'code')
     end
 
+    test '#resolve returns specified offering to admins even if they are not enrolled' do
+      offering_id = offerings(:quality_assurance_A).id
+      query = <<~EOF
+        query Offering {
+          offering(id: #{offering_id}) {
+            section
+            course {
+              code
+            }
+          }
+        }
+      EOF
+
+      results = CmsSchema.execute(query, context: { current_user: users(:admin) }, variables: {}).to_h
+      offering = results.dig('data', 'offering')
+      assert_equal offerings(:quality_assurance_A).section, offering['section']
+      assert_equal offerings(:quality_assurance_A).course.code, offering.dig('course', 'code')
+    end
+
     test '#resolve returns nil when specified offering does not exist' do
       query = <<~EOF
         query Offering {
@@ -54,6 +73,24 @@ module Resolvers
       EOF
 
       results = CmsSchema.execute(query, context: {}, variables: {}).to_h
+
+      assert_nil results.dig('data', 'offering')
+    end
+
+    test '#resolve returns nil if the user is not enrolled in the offering' do
+      offering_id = offerings(:quality_assurance_A).id
+      query = <<~EOF
+        query Offering {
+          offering(id: #{offering_id}) {
+            section
+            course {
+              code
+            }
+          }
+        }
+      EOF
+
+      results = CmsSchema.execute(query, context: { current_user: users(:not_admin2) }, variables: {}).to_h
 
       assert_nil results.dig('data', 'offering')
     end
