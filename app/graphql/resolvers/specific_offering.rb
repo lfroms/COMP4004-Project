19c83c1d9e4cf5e9052a546ea23fd168f578ddcc
Lukas Romsicki <lukas@romsicki.com>
2020-12-02 21:54:37 -0500

    [CODE] Add implementation

--- app/graphql/resolvers/specific_offering.rb
+++ app/graphql/resolvers/specific_offering.rb
@@ -8,7 +8,17 @@ module Resolvers
     def resolve(id:)
       assert_authenticated!
 
-      Offering.find_by(id: id)
+      offering = Offering.find_by(id: id)
+      return nil unless offering
+      return nil unless user_can_access_offering(offering)
+
+      offering
+    end
+
+    private
+
+    def user_can_access_offering(offering)
+      context[:current_user].admin || offering.participants.include?(context[:current_user])
     end
   end
 end
