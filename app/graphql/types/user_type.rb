8ab5861b55bec0971fbd899c9496032273d3bb5e
Lukas Romsicki <lukas@romsicki.com>
2020-12-03 22:44:59 -0500

    [CODE] Add enrollment filtering to UserType

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -6,6 +6,15 @@ module Types
     field :email, String, null: false
     field :admin, Boolean, null: false
     field :approved, Boolean, null: false
-    field :enrollments, Types::EnrollmentType.connection_type, null: false
+    field :enrollments, Types::EnrollmentType.connection_type, null: false do
+      argument :offering_id, ID, required: false
+      argument :role, Types::EnrollmentRoleType, required: false
+    end
+
+    def enrollments(args = nil)
+      return object.enrollments unless args
+
+      object.enrollments.where(args)
+    end
   end
 end
