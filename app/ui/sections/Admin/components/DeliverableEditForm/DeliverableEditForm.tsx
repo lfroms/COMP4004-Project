e3488aa5003cd390c022c447575d56e227365802
Mitchell Soares <mitchell.soares@shopify.com>
2020-12-05 15:23:57 -0500

    [CODE] Added deliverable modal + form

--- app/ui/sections/Admin/components/DeliverableEditForm/DeliverableEditForm.tsx
+++ app/ui/sections/Admin/components/DeliverableEditForm/DeliverableEditForm.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Form} from 'antd';
+import { DatePicker, Form, Input, InputNumber } from 'antd';
 
 export interface DeliverableEditFormData {
   title: string;
   description: string;
   weight: number;
-  dueData: Date;
+  dueDate: Date;
 }
 
 interface Props {
@@ -14,14 +14,25 @@ interface Props {
 }
 
 export default function DeliverableEditForm({ name, onSubmit }: Props) {
-
   return (
     <Form name={name} onFinish={onSubmit}>
+      <Form.Item name="title" rules={[{ required: true, message: 'You must enter a title' }]}>
+        <Input id="deliverable_title_field" placeholder="Title" />
+      </Form.Item>
       <Form.Item
-        name="title"
-        label="Title"
+        name="description"
+        rules={[{ required: true, message: 'You must enter a description' }]}
       >
-
+        <Input id="deliverable_description_field" placeholder="Description" />
+      </Form.Item>
+      <Form.Item
+        name="weight"
+        rules={[{ required: true, message: 'You must enter a weight' }, { max: 1 }, { min: 0 }]}
+      >
+        <InputNumber id="deliverable_weight_field" placeholder="Weight" />
+      </Form.Item>
+      <Form.Item name="dueDate" rules={[{ required: true, message: 'You must enter a due date' }]}>
+        <DatePicker />
       </Form.Item>
     </Form>
   );
---
+++ app/ui/sections/Admin/components/DeliverableEditForm/index.ts
@@ -0,0 +1,2 @@
+export { default as DeliverableEditForm } from './DeliverableEditForm';
+export { DeliverableEditFormData } from './DeliverableEditForm';
