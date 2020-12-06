c907e11082fa03cd2c894302a840095e2a8fb60b
Lukas Romsicki <lukas@romsicki.com>
2020-12-05 19:30:22 -0500

    [REF] Rubocop

--- config/initializers/validates_associated_attributes.rb
+++ config/initializers/validates_associated_attributes.rb
@@ -1,13 +1,16 @@
 # frozen_string_literal: true
 module ValidatesAssociatedAttributes
+  # rubocop:disable Style/ClassAndModuleChildren
   module ActiveRecord::Validations::ClassMethods
+    # rubocop:enable Style/ClassAndModuleChildren
     def validates_associated(*associations)
       class_eval do
         validates_each(associations) do |record, _associate_name, value|
           (value.respond_to?(:each) ? value : [value]).each do |rec|
             next unless rec && !rec.valid?
-            rec.errors.each do |key, value|
-              record.errors.add(key, value)
+
+            rec.errors.each do |field, message|
+              record.errors.add(field, message)
             end
           end
         end
