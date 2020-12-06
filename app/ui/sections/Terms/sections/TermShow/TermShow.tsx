8c854d2e0b920c6dfaafd985f5cc5bb20290de04
Gaby Pancu <gaby.pancu@shopify.com>
2020-12-06 17:35:04 -0500

    add enroll button for self-enrolling users

--- app/ui/sections/Terms/sections/TermShow/TermShow.tsx
+++ app/ui/sections/Terms/sections/TermShow/TermShow.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
 import { NavigationGroup, Page, TitleBar } from 'components';
-import { Empty, Table } from 'antd';
-import { CalendarOutlined } from '@ant-design/icons';
+import { Button, Empty, Popconfirm, Table } from 'antd';
+import { CalendarOutlined, PlusSquareOutlined } from '@ant-design/icons';
 import { createTermName } from 'helpers';
 import { ColumnType } from 'antd/lib/table';
-
+import { TermCurrentUserQuery } from './graphql/TermCurrentUserQuery';
 import { TermShowQuery, TermShowQuery_terms_nodes_offerings_nodes } from './graphql/TermShowQuery';
 
 import * as styles from './TermShow.module.scss';
@@ -41,11 +41,35 @@ export default function TermShow() {
     }
   `;
 
+  const USER_CAN_SELF_ENROLL = gql`
+    query TermCurrentUserQuery {
+      currentUser {
+        canSelfEnroll
+        enrollments {
+          nodes {
+            id
+          }
+        }
+      }
+    }
+  `;
+
+  //   const CREATE_ENROLLMENT = gql`
+  //   mutation TermShowEnrollmentCreationMutation($id: ID!) {
+  //     createEnrollment(input: { id: $id }) {
+  //       enrollment {
+  //         id
+  //       }
+  //     }
+  //   }
+  // `;
+
   const history = useHistory();
-  const { data } = useQuery<TermShowQuery>(TERMS_WITH_OFFERINGS);
+  const { data: data1 } = useQuery<TermShowQuery>(TERMS_WITH_OFFERINGS);
+  const { data: data2 } = useQuery<TermCurrentUserQuery>(USER_CAN_SELF_ENROLL);
 
   const groups: NavigationGroup[] =
-    (data?.terms.nodes
+    (data1?.terms.nodes
       ?.map(term => {
         if (!term) {
           return null;
@@ -69,6 +93,13 @@ export default function TermShow() {
     </div>
   );
 
+  // const [enroll, { loading: enrollLoading }] = useMutation<
+  //   TermShowEnrollMutation,
+  //   AdminTermShowEnrollMutationVariables
+  // >(ENROLL, {
+  //   refetchQueries: ['TermShowQuery'],
+  // });
+
   const columns: ColumnType<TermShowQuery_terms_nodes_offerings_nodes>[] = [
     {
       title: 'Name',
@@ -90,7 +121,28 @@ export default function TermShow() {
     },
   ];
 
-  const currentTerm = data?.terms.nodes?.find(term => term?.id === termId);
+  if (data2?.currentUser?.canSelfEnroll) {
+    columns.push({
+      key: 'actions',
+      fixed: 'right',
+      align: 'right',
+      // render: (_text, record) => (
+      render: () => (
+        <Popconfirm
+          title="Are you sure you want to enroll in this course?"
+          placement="rightBottom"
+          // onConfirm={handleConfirmEnroll(record.id)}
+          okText="Confirm"
+          cancelText="Cancel"
+          // okButtonProps={{ loading: deleteLoading }}
+        >
+          <Button icon={<PlusSquareOutlined />} />
+        </Popconfirm>
+      ),
+    });
+  }
+
+  const currentTerm = data1?.terms.nodes?.find(term => term?.id === termId);
   const offerings = currentTerm?.offerings.nodes?.filter(Boolean) ?? [];
 
   return (
