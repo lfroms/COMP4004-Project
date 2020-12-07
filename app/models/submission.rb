d381ca598985683e634e0fec91b3e2e10c411c52
Lukas Romsicki <lukas@romsicki.com>
2020-12-06 21:28:57 -0500

    [CODE] Prevent multiple submissions from same user

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,6 +2,7 @@
 class Submission < ApplicationRecord
   URL_REGEXP = %r{\A(http|https)://[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,63}(:[0-9]{1,5})?(/.*)?\z}ix
   validates :attachment_url, presence: true, format: { with: URL_REGEXP }
+  validates :user_id, uniqueness: { scope: :deliverable_id }
 
   belongs_to :deliverable
   belongs_to :user
