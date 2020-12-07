261c97fdefc00ae27893e722f9a4f78cf25747c9
Lukas Romsicki <lukas@romsicki.com>
2020-12-06 22:41:53 -0500

    [REF] Extract card into component

--- app/ui/sections/Enrollments/sections/EnrollmentShow/sections/Dashboard/Dashboard.tsx
+++ app/ui/sections/Enrollments/sections/EnrollmentShow/sections/Dashboard/Dashboard.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { gql, useQuery } from '@apollo/client';
 import { useHistory, useParams } from 'react-router-dom';
 import { TitleBar } from 'components';
-import { Card, Space, Typography } from 'antd';
+import { Space } from 'antd';
+import { DeliverableCard } from './components';
 
 import { DashboardQuery, DashboardQueryVariables } from './graphql/DashboardQuery';
-import { createFriendlyDate } from 'helpers';
 
 const DELIVERABLES = gql`
   query DashboardQuery($offeringId: ID!) {
@@ -39,25 +39,18 @@ export default function Dashboard() {
   });
 
   const deliverables = data?.offering?.deliverables.nodes?.map((deliverable, index) => {
-    const title = (
-      <>
-        <Typography.Text>{deliverable?.title}</Typography.Text>
-        <Typography.Text type="secondary">
-          {' '}
-          - Due {createFriendlyDate(deliverable?.dueDate)}
-        </Typography.Text>
-      </>
-    );
+    if (!deliverable) {
+      return null;
+    }
 
     return (
-      <Card
+      <DeliverableCard
         key={`dashboard-deliverable-${index}`}
-        cover={<div style={{ backgroundColor: '#ddd', height: 70 }} />}
-        onClick={() => history.push(`${offeringId}/deliverables/${deliverable?.id}`)}
-        hoverable
-      >
-        <Card.Meta title={title} description={deliverable?.description} />
-      </Card>
+        title={deliverable.title}
+        description={deliverable.description}
+        dueDate={deliverable.dueDate}
+        onClick={() => history.push(`${offeringId}/deliverables/${deliverable.id}`)}
+      />
     );
   });
 
