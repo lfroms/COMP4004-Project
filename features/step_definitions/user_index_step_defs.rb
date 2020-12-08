db41a8e5d14a38f2e78e305463f0c0f90ecbb1d4
Mitchell Soares <mitchell.soares@shopify.com>
2020-12-08 10:40:02 -0500

    [ACCEPTANCE TEST] Updated account approval feature

--- features/step_definitions/user_index_step_defs.rb
+++ features/step_definitions/user_index_step_defs.rb
@@ -2,17 +2,18 @@
 
 Given('there exists a user with email {string} and approved status {string}') do |string, string2|
   approved = string2 == 'true' ? true : false
-  User.create(name: 'Not Approved', email: string, approved: approved)
+  User.create!(name: 'Not Approved', email: string, password: '123456', approved: approved)
 end
 
-Given('I am on the user index') do
+And('I am on the user index') do
   wait_for_page_load
   visit('/admin/users')
   wait_for_page_load
 end
 
-When('I click the approve button for the user') do
-  button = find('.approve_user_button').first
+And('I click the approve button for the user') do
+  user_id = User.find_by(name: 'Not Approved').id
+  button = find("#approve_user_id_#{user_id}")
   button.click
 end
 
