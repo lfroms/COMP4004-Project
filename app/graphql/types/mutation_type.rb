a64153386f4c06083e7990578d2b0cb830aea52f
Lukas Romsicki <lukas@romsicki.com>
2020-12-07 21:57:52 -0500

    [CODE] Add implementation for group creation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -8,6 +8,8 @@ module Types
     field :update_user, mutation: Mutations::UpdateUser
     field :register_user, mutation: Mutations::RegisterUser
 
+    field :create_group, mutation: Mutations::CreateGroup
+
     field :create_course, mutation: Mutations::CreateCourse
     field :update_course, mutation: Mutations::UpdateCourse
     field :delete_course, mutation: Mutations::DeleteCourse
