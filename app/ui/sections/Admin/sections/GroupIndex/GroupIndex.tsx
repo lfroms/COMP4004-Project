6d29befbbf5ff3faca5c6bfdbc4d2f5019f8473b
Lukas Romsicki <lukas@romsicki.com>
2020-12-09 16:29:39 -0500

    [CODE] Add modal to create groups

--- app/ui/sections/Admin/sections/GroupIndex/GroupIndex.tsx
+++ app/ui/sections/Admin/sections/GroupIndex/GroupIndex.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Table } from 'antd';
+import { AppstoreAddOutlined } from '@ant-design/icons';
 import { gql, useQuery } from '@apollo/client';
-import { AdminGroupsQuery, AdminGroupsQuery_groups_nodes } from './graphql/AdminGroupsQuery';
 import { ColumnType } from 'antd/lib/table';
 import { TitleBar } from 'components';
+import { GroupCreateModal } from 'sections/Admin/components';
+
+import { AdminGroupsQuery, AdminGroupsQuery_groups_nodes } from './graphql/AdminGroupsQuery';
 
 const ALL_GROUPS = gql`
   query AdminGroupsQuery {
@@ -22,6 +25,7 @@ const ALL_GROUPS = gql`
 `;
 
 export default function GroupIndex() {
+  const [groupCreateModalVisible, setGroupCreateModalVisible] = useState(false);
   const { data, loading } = useQuery<AdminGroupsQuery>(ALL_GROUPS);
 
   const columns: ColumnType<AdminGroupsQuery_groups_nodes>[] = [
@@ -44,12 +48,28 @@ export default function GroupIndex() {
 
   return (
     <>
-      <TitleBar title="Groups" />
+      <TitleBar
+        title="Groups"
+        actions={[
+          {
+            elementId: 'create_group',
+            icon: <AppstoreAddOutlined />,
+            text: 'New group',
+            onClick: () => setGroupCreateModalVisible(true),
+          },
+        ]}
+      />
+
       <Table
         columns={columns}
         dataSource={groups as AdminGroupsQuery_groups_nodes[]}
         pagination={false}
         loading={loading}
+      />
+
+      <GroupCreateModal
+        visible={groupCreateModalVisible}
+        onRequestClose={() => setGroupCreateModalVisible(false)}
       />
     </>
   );
