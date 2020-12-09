06bca1f7e079b681377395fa9b4cc69204899881
Gaby Pancu <gaby.pancu@shopify.com>
2020-12-09 15:22:34 -0500

    linting

--- db/migrate/20201209201940_remove_financial_deadline_from_terms.rb
+++ db/migrate/20201209201940_remove_financial_deadline_from_terms.rb
@@ -1,5 +1,6 @@
+# frozen_string_literal: true
 class RemoveFinancialDeadlineFromTerms < ActiveRecord::Migration[6.0]
   def change
-    remove_column :terms, :financial_deadline, :datetime
+    remove_column(:terms, :financial_deadline, :datetime)
   end
 end
