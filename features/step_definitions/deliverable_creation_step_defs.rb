3144a546e49c09747a606083b7ea9ecfa7fb46ca
mitchmaps <42747959+mitchmaps@users.noreply.github.com>
2020-12-10 22:41:42 -0500

    [ACCEPTANCE TEST] Added submission_grading step defs (#305)

--- features/step_definitions/deliverable_creation_step_defs.rb
+++ features/step_definitions/deliverable_creation_step_defs.rb
@@ -15,6 +15,13 @@ When('I enter deliverable due date {string}') do |string|
   fill_in('deliverable_due_date_field', with: string)
 end
 
+When('there exists a deliverable called {string} for course offering for course with code {string} with section {string}') do |string, string2, string3|
+  course = Course.find_by(code: string2)
+  offering = Offering.find_by(course: course, section: string3)
+
+  Deliverable.create!(title: string, weight: 0.15, description: 'this is a deliverable', due_date: Time.zone.local(2020, 12, 1, 4, 5, 6), offering: offering)
+end
+
 Then('there exists a deliverable with title {string} with the description {string}') do |title, description|
   assert_text title
   assert_text description
