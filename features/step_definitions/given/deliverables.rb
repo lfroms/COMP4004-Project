cd47415973d9054fff598d9317854d7c12bdce8d
Mitchell Soares <mitchell.soares@shopify.com>
2020-12-12 15:32:00 -0500

    [ACCEPTANCE TEST] Added scenario for a past due submission

--- features/step_definitions/given/deliverables.rb
+++ features/step_definitions/given/deliverables.rb
@@ -13,3 +13,10 @@ Given('there exists a deliverable called {string} with due date later than today
 
   Deliverable.create!(title: title, weight: 0.15, description: 'this is a deliverable', due_date: Time.zone.today + 1, offering: offering)
 end
+
+Given('there exists a deliverable called {string} with due date one day ago for course offering for course with code {string} section {string} for the current term') do |title, code, section|
+  course = Course.find_by(code: code)
+  offering = Offering.find_by(course: course, section: section, term: @term)
+
+  Deliverable.create!(title: title, weight: 0.15, description: 'this is a deliverable', due_date: Time.zone.today - 1, offering: offering)
+end
